21c5c63b2e5290390ab20e4b58cce40c61117c7c
R A S H <55981293+theRishu@users.noreply.github.com>
2021-05-20 06:23:23 +0530

    Update config.py

--- randtalkbot/config.py
+++ randtalkbot/config.py
@@ -1,4 +1,7 @@
+import os
+
 path = 'configuration.json'
 database_password = 1234
 token = "1705941714:AAEDdjZLAMXnE-wl9X8FhtdpQJ7j0loKdro"
-DB_URL = "postgres://nijtgyjdsthdiw:23933ecc21dcf1f6daf5083372ae333f90294cf07b0b86b869cbf56763749f95@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d78u9g1h7qiaf0"
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
