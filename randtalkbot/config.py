616d97805c9916e21af842cd65eea40aa97ad5b4
jamalnggau1 <44499007+jamalnggau1@users.noreply.github.com>
2021-09-30 23:54:50 +0800

    Update config.py

--- randtalkbot/config.py
+++ randtalkbot/config.py
@@ -2,6 +2,6 @@ import os
 
 path = 'configuration.json'
 database_password = 1234
-token = "1705941714:AAEDdjZLAMXnE-wl9X8FhtdpQJ7j0loKdro"
+token = "1953016364:AAEr_BwhecXg_dTSzbPF5W9_lQhHZltXl1I"
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
